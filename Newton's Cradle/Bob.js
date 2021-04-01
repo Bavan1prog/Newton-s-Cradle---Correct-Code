a28dba836dd902b92e1e92e30ee3193447f2dbcc
Bavan1prog <63508479+Bavan1prog@users.noreply.github.com>
2021-04-01 17:03:05 +0530

    Add files via upload

---
+++ Newton's Cradle/Bob.js
@@ -0,0 +1,23 @@
+class BOB{
+    constructor(x,y){
+        var options = {
+            isStatic : false,
+            restitution : 0.3,
+            friction: 0.5,
+            density: 1.5
+        }
+        this.body = Bodies.circle(x,y,20,options);
+        this.radius = 20;
+        World.add(world,this.body);
+    }
+    display(){
+        var pos = this.body.position;
+        push();
+        ellipseMode(RADIUS);
+        fill("red");
+        ellipse(pos.x,pos.y,this.radius,this.radius);
+        pop();
+    }
+
+
+}
